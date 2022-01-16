f803771f5afdcba3c618ada5a4f1f9b49476eae7
sehbazz virk <sehbazz@ualberta.ca>
2022-01-15 19:44:35 -0700

    added U2U transaction.js

---
+++ hello-hedera-js-sdk/U2Utransaction.js
@@ -0,0 +1,13 @@
+async function U2Utransaction(tokenID, quatID, sellerID, buyerID, 
+                              sellerKey, buyerKey, price, client) {
+    let tokenTransfer = await new TransferTransaction()
+        .addNftTransfer(tokenID, quatID, sellerID, buyerID)
+        .addHbarTransfer(sellerID, price)
+        .addHbarTransfer(buyerID, -price)
+        .freezeWith(client)
+        .sign(sellerKey);
+    tokenTranserSign = await tokenTransfer.sign(buyerKey);
+    let tokenTransferSubmit = await tokenTransferSign.execute(client);
+    let tokenTransferRx = await tokenTransferSubmit.getReceipt(client);
+    console.log(`\n Kumquat transfer ${sellerID} to ${BuyerID} status: ${tokenTransferRx.status} \n`);
+}
