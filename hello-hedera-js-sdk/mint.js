2991c890f28575acc68618fe52d31ad60949791c
sehbazz virk <sehbazz@ualberta.ca>
2022-01-15 19:07:41 -0700

    updates to mint.js

--- hello-hedera-js-sdk/mint.js
+++ hello-hedera-js-sdk/mint.js
@@ -2,10 +2,19 @@ QUATS = [
     "Hog Ridah",
     "Mashallah Sister Dixie",
     "Sorry Martha no one wants you in their bloodline",
-    "Jesus Died For These Quads"
-]
+    "Jesus Died For These Quads",
+    "If You Like it then you should've put a ring on it",
+    "Top of tha morning",
+    "Get Kummed On",
+    "i'm a lolicon",
+    "Friction makes things more spicy",
+    "AMOGUS",
+    "Loyal to Alabama",
+    "Oppresion leads to good music",
+    "A MAN HAS FALLEN INTO THE RIVER IN LEGO CITY",
+];
 
-async function main() {
+async function mintKumquat(tokenId, client, supplyKey) {
 
     mintTx = await new TokenMintTransaction()
 			.setTokenId(tokenId)
@@ -16,3 +25,5 @@ async function main() {
     let mintRx = await mintTxSubmit.getReceipt(client);
     return mintRx;
 }
+
+export default mintKumquat;
