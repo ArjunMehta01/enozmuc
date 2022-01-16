e9d8f984bda55b718fa6eb9c6101e6567f18add3
Albert Dinh <albertdinh@Albert-Dinhs-MacBook-Pro.local>
2022-01-15 23:43:52 -0700

    Fixed structure and frontend

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-
+import { Landing } from "./components/Landing";
 function App() {
-	return <Navbar></Navbar>;
+	return (
+		<>
+			<Navbar></Navbar>
+			<Landing></Landing>
+		</>
+	);
 }
 
 export default App;
---
+++ front_end/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Landing.css";
+
+export const Landing = () => {
+	return (
+		<div className="container-size">
+			<p className="heading_name">Welcome to ENOZMUC</p>
+			<p className="intro-para">ENOZMUC is a test sandbox or simulation</p>
+		</div>
+	);
+};
