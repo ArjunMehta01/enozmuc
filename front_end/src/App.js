1c9195036eb42d8850ea4c64ec2e720667871fc3
arjun <arjun.mehta1001@gmail.com>
2022-01-16 09:21:43 -0700

    angular start

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,12 +1,50 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import React, {useState} from "react";
+
+
+import { Navbar } from "./components/UI/Navbar";
 import { Landing } from "./components/Landing";
+import Card from "./components/UI/Card";
+import Sandbox from "./components/Sandbox";
+
+
+import {Container, Row, Col} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+
 function App() {
+	const testusers = ["pubKey: 32324, privKey: 3232423, accId: 2342", "pubKey: 32324, privKey: 3232423, accId: 2342"];
+	const [generate, setGenerate] = useState(true);
+
+
+
+	const generateHandler = () => {
+		setGenerate(false);
+	}
+
+
 	return (
-		<>
-			<Navbar></Navbar>
-			<Landing></Landing>
-		</>
+		<div>
+			{generate ? <Button onClick={generateHandler}>cum</Button> : <Sandbox/>}
+
+
+
+
+			{/* <Navbar></Navbar>
+			<Landing></Landing> */}
+			{/* <Card>
+				<Button>Cum</Button>
+			</Card>
+			<Card>
+				<ListRenderer list={[...test]}/>
+			</Card> */}
+
+
+			
+
+
+
+
+		</div>
 	);
 }
 
